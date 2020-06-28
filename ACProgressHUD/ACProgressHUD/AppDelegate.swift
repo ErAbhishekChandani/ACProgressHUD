2507b7ca09f4481e979d9b2fffebfb67a5d7f325
Abhishek <chandani.abhishek440@gmail.com>
2020-06-28 22:21:10 +0530

    Swift Guide update

--- ACProgressHUD/ACProgressHUD/AppDelegate.swift
+++ ACProgressHUD/ACProgressHUD/AppDelegate.swift
@@ -12,13 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Configure the Progress HUD one time for using in App.
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         ACProgressHUD.shared.configureStyle(withProgressText: "Hey Please wait...", progressTextColor: UIColor.black, progressTextFont: UIFont.systemFont(ofSize: 17),shadowColor: UIColor.black, shadowRadius: 10, cornerRadius: 5, indicatorColor: UIColor.orange, hudBackgroundColor: UIColor.white, enableBackground: false, backgroundColor: UIColor.black, backgroundColorAlpha: 0.3, enableBlurBackground: false,showHudAnimation: .growIn,dismissHudAnimation: .growOut)
         return true
     }
-    
-    
 }
 
